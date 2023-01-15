9c94e13ce514acc3c8af40ab320ae0e45ad039a2
Max Smirnov <smirnov.m4ksim@gmail.com>
2023-01-15 14:26:24 +0200

    add description and usage

--- gendiff.js
+++ gendiff.js
@@ -2,7 +2,13 @@ const { program } = require('commander');
 
 program
     .name('gendiff')
+    .usage('[options] <filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number');
+    .option('-V, --version', 'output the version number')
+    .option('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
+    .parse();
 
-program.parse();
+const options = program.opts();
+
+if (options.help) program.help()
