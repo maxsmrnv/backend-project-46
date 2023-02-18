cf296440b66916e2a7d4c363ab4e7b9652ac217c
Max Smirnov <smirnov.m4ksim@gmail.com>
2023-02-18 16:59:20 +0200

    fix no-rest-parameters use

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,9 +1,15 @@
-const iter = (diffTree) => diffTree.reduce((acc, next) => {
-  const {
-    children, key, type, ...props
-  } = next;
-  if (children) return { ...acc, [key]: { value: iter(next.children), type } };
-  return { ...acc, [key]: { ...props, type } };
+const iter = (diffTree) => diffTree.reduce((jsonTree, node) => {
+  switch (node.type) {
+    case 'nested':
+      return { ...jsonTree, [node.key]: { value: iter(node.children), type: 'nested' } };
+    case 'updated':
+      return { ...jsonTree, [node.key]: { prevValue: node.prevValue, value: node.value, type: 'updated' } };
+    case 'unchanged':
+    case 'removed':
+    case 'added':
+    default:
+      return { ...jsonTree, [node.key]: { value: node.value, type: node.type } };
+  }
 }, {});
 const formatDiff = (diff) => JSON.stringify(iter(diff));
 
